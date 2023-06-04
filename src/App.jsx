afa76d3c6ca281c68360d6bf6e51dc841fa6ac79
ashishxcode <ashish.patel0903@gmail.com>
2023-06-04 23:03:14 +0530

    CI : Display Section

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Jumbotron from "./components/Jumbotron";
 import SoundFeature from "./components/SoundFeature";
+import DisplaySection from "./components/DisplaySection";
 const App = () => {
 	return (
 		<div id="App">
 			<Navbar />
 			<Jumbotron />
 			<SoundFeature />
+			<DisplaySection />
 		</div>
 	);
 };
