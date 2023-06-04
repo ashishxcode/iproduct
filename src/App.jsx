867715d27df120bf8cea84ee09520fd03323ea2a
ashishxcode <ashish.patel0903@gmail.com>
2023-06-04 22:45:31 +0530

    CI : Sound Feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Jumbotron from "./components/Jumbotron";
+import SoundFeature from "./components/SoundFeature";
 const App = () => {
 	return (
 		<div id="App">
 			<Navbar />
 			<Jumbotron />
+			<SoundFeature />
 		</div>
 	);
 };
---
+++ src/components/SoundFeature.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const SoundFeature = () => {
+	return (
+		<div className="sound-section wrapper">
+			<div className="body">
+				<div className="sound-section-content content">
+					<h2 className="title">New Sound System</h2>
+					<p className="text">Feel the base</p>
+					<span className="description">
+						From $799 or $33.29/mo. for 24 mo. before trade‑in
+					</span>
+					<ul className="links">
+						<li>
+							<button className="button">Buy</button>
+						</li>
+						<li>
+							<a className="link" href="#">
+								Learn more
+							</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default SoundFeature;
